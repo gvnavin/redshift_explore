14f57915f299e1e1a1fed2f7bbc451a6266b8b59
Navin G V <gnavin@amazon.com>
2017-09-06 21:46:50 +0530

    adding insert into query

--- src/main/java/com/gnavin/redshift/Redshift.java
+++ src/main/java/com/gnavin/redshift/Redshift.java
@@ -15,11 +15,52 @@ public class Redshift {
         //props.setProperty("ssl", "true");
         props.setProperty("user", inputProp.getProperty("user"));
         props.setProperty("password", inputProp.getProperty("pass"));
-    
-        //Dynamically load driver at runtime.
-        //Redshift JDBC 4.1 driver: com.amazon.redshift.jdbc41.Driver
-        //Redshift JDBC 4 driver: com.amazon.redshift.jdbc4.Driver
+        
         Class.forName("com.amazon.redshift.jdbc42.Driver");
+    }
+    
+    public void insertIntoTables() throws SQLException {
+        Connection conn = null;
+        try {
+            
+            //Dynamically load driver at runtime.
+            //Redshift JDBC 4.1 driver: com.amazon.redshift.jdbc41.Driver
+            //Redshift JDBC 4 driver: com.amazon.redshift.jdbc4.Driver
+            Class.forName("com.amazon.redshift.jdbc42.Driver");
+            //Open a connection and define properties.
+            System.out.println("Connecting to database...");
+            
+            conn = DriverManager.getConnection(inputProp.getProperty("dbURL"), props);
+            
+            //https://www.mkyong.com/jdbc/jdbc-preparestatement-example-insert-a-record/
+            String insertTableSQL = "INSERT INTO attempt_stats"
+                + "(encrypted_customer_id, examtype_subject_topic_level_attempt_x, score) "
+                + "VALUES"
+                + "(?,?,?)";
+            
+            try (PreparedStatement preparedStatement = conn.prepareStatement(insertTableSQL)) {
+                preparedStatement.setString(1, "A2D3K6KMQA9XQJ");
+                preparedStatement.setString(2, "iit-jee_physics_kinematics_easy_attempt_1");
+                preparedStatement.setInt(3, 10);
+                preparedStatement.executeUpdate();
+                preparedStatement.close();
+            }
+            
+            System.out.println("Insert into the table finished");
+            conn.close();
+            
+        } catch (Exception ex) {
+            //For convenience, handle all errors here.
+            ex.printStackTrace();
+        } finally {
+            //Finally block to close resources.
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
     
     public void printTables() {
@@ -30,6 +71,9 @@ public class Redshift {
             //Open a connection and define properties.
             System.out.println("Connecting to database...");
             
+            //Dynamically load driver at runtime.
+            //Redshift JDBC 4.1 driver: com.amazon.redshift.jdbc41.Driver
+            //Redshift JDBC 4 driver: com.amazon.redshift.jdbc4.Driver
             conn = DriverManager.getConnection(inputProp.getProperty("dbURL"), props);
             
             //Try a simple query.
@@ -38,13 +82,13 @@ public class Redshift {
             String sql;
             sql = "select * from information_schema.tables;";
             try (ResultSet rs = stmt.executeQuery(sql)) {
-        
+                
                 //Get the data from the result set.
                 while (rs.next()) {
                     //Retrieve two columns.
                     String catalog = rs.getString("table_catalog");
                     String name = rs.getString("table_name");
-    
+                    
                     //Display values.
                     System.out.print("Catalog: " + catalog);
                     System.out.println(", Name: " + name);
